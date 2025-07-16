f483fe259af4f8646aa4175be01ada32e112cebf
lamanh-lavain <lavainse@gmail.com>
2025-07-16 22:51:23 +0700

    Update the UI. In: src/pages/product_detail/components/product_extra.jsx.

--- Frontend-EC/src/pages/product_detail/components/product_extra.jsx
+++ Frontend-EC/src/pages/product_detail/components/product_extra.jsx
@@ -4,11 +4,13 @@ import { accessories, bonus_gifts } from '../../../data/dummy'
 function Extra_Item({extra}) {
 
   return (
-    <div className='flex flex-col justify-around items-center px-8 w-[180px] h-[100px] bg-white text-black'>
-        <img src={extra.image_url}/>
-        <div className='flex'>
-          <p className='font-bold text-2xl'>{extra.name}</p>
-          <p className='font-medium text-sm'>{extra.price}</p>
+    <div className='flex justify-around items-center min-w-[150px] h-[110px] border-1'>
+        <div className='h-[100%] aspect-square overflow-hidden'>
+          <img src={extra.image_url} className='w-full h-full object-cover'/>
+        </div>
+        <div className='flex flex-col px-2'>
+          <p className='font-light text-sm'>{extra.name}</p>
+          <p className='font-extralight text-sm'>+${extra.price}</p>
         </div>
     </div>
   )
@@ -19,7 +21,7 @@ function Product_Extra({product}) {
   const extras = accessories.filter(accessory => matched_accessories_id.includes(accessory.product_id))
 
   return (
-    <div className="flex">
+    <div className="flex gap-1 py-6 justify-around">
         {extras.map((extra) => (
             <Extra_Item key={extra.product_id} extra={extra}/>
         ))}
