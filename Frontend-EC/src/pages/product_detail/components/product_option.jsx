b7fbb0f77da17ae04228111839abef47bfeba09d
lamanh-lavain <lavainse@gmail.com>
2025-07-16 22:50:07 +0700

    Update the UI. In: src/pages/product_detail/components/product_option.jsx.

--- Frontend-EC/src/pages/product_detail/components/product_option.jsx
+++ Frontend-EC/src/pages/product_detail/components/product_option.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Option_Item({option}) {
     return (
-    <div className='flex flex-col justify-around items-center px-8 w-[180px] h-[100px] bg-white text-black'>
-        <p className='font-bold text-2xl'>{option.name}</p>
-        <p className='font-medium text-sm'>{option.price}</p>
-        <p className='font-extralight text-sm text-gray-500'>{option.stems}</p>
+    <div className='max-w-[180px] w-full h-[120px] flex flex-col justify-around items-center py-5 border-1'>
+       <p className='font-bold text-xl'>${option.price}</p>
+        <p className='font-medium text-xs'>{option.name.toUpperCase()}</p>
+        <p className='font-extralight text-xs text-gray-500'>{option.stems} stems</p>
     </div>
   )
 }
@@ -14,7 +14,7 @@ function Option_Item({option}) {
 function Product_Option({product}) {
   console.log(product.product_id)
   return (
-    <div className="flex">
+    <div className="flex justify-between gap-2">
         {Object.keys(product.options).map((name) => (
             <Option_Item key={name} option={{name:name, price:product.options[name].price, stems:product.options[name].stems}}/>
         ))}
