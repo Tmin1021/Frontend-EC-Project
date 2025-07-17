843c53f18edb66346302c6521792d69f09e0fe3f
lamanh-lavain <lavainse@gmail.com>
2025-07-17 20:29:40 +0700

    Add relating to Search and Filter Product functions. In: src/context/ProductContext.jsx.

--- Frontend-EC/src/context/ProductContext.jsx
+++ Frontend-EC/src/context/ProductContext.jsx
@@ -5,24 +5,53 @@ const ProductContext = createContext()
 
 export function ProductProvider({children}) {
   const [current_product, setCurrentProduct] = useState(products)
-  const [current_values, setCurrentValues] = useState({"Flower Type":"", "Occassions": "", "Colors": "", "Sort": ""})
+  const [current_values, setCurrentValues] = useState({"Flower Type": new Set([""]), "Occassions": new Set([""]), "Colors": new Set([""]), "Sort": new Set([""])})
 
-  const filterProduct = ({type, value}) => { 
+  const filterProduct = ({type, value, isChosen}) => { 
     let new_product = products
     let new_values = current_values
 
-    new_values[type] = value
-    new_product = (new_values["Flower Type"]!=="" ? products.filter((product) => product.flower_type === new_values["Flower Type"]) : new_product)
-    new_product = (new_values["Occassions"]!=="" ? new_product.filter((product) => product.occasion === new_values["Occassions"]) : new_product)
-    new_product = (new_values["Colors"]!=="" ? new_product.filter((product) => product.color === new_values["Colors"]) : new_product)
-
+    isChosen ? new_values[type].add(value) : new_values[type].delete(value)
+    new_product = (new_values["Flower Type"].size!==1 ? products.filter((product) => new_values["Flower Type"].has(product.flower_type)) : new_product)
+    new_product = (new_values["Occassions"].size!==1 ? new_product.filter((product) => new_values["Occassions"].has(product.occasion)) : new_product)
+    new_product = (new_values["Colors"].size!==1? new_product.filter((product) => new_values["Colors"].has(product.color)) : new_product)
+    console.log(new_values)
+    
     setCurrentValues(new_values)
     setCurrentProduct(new_product)
-    
   }
 
+  const searchProduct = (input) => {
+    const matches = [
+      ...products.filter(product => product.name.includes(input)),
+      ...products.filter(product => product.flower_type.includes(input)),
+      ...products.filter(product => product.occasion.includes(input)),
+      ...products.filter(product => product.color.includes(input)),
+    ]
+
+    const uniqueMatches = Array.from(new Set(matches))
+    return uniqueMatches
+  }
+
+  const searchPrediction = (input) => {
+    const predictions = []
+
+    products.forEach((product) => {
+      const name = product.name;
+      const index = name.toLowerCase().indexOf(input.toLowerCase())
+
+      if (index !== -1 && index + input.length < name.length) {
+        const remaining = name.slice(index + input.length)
+        if (name.toLowerCase().split(" ").includes((input+remaining).split(" ")[0])) {predictions.push(remaining.trim())}
+      }
+    })
+
+    return predictions[0]
+  }
+
+
   return (
-    <ProductContext.Provider value={{current_product, filterProduct}}>
+    <ProductContext.Provider value={{current_product, filterProduct, searchProduct, searchPrediction}}>
       {children}
     </ProductContext.Provider>
   )
